ff1ba1b615a94cb2141b8a3a0d28771bbf0a0f66
Deran Schilling <deran@headspring.com>
2013-05-18 12:25:19 -0500

    Modified query to return only food types of places

--- src/zindler.web/Core/Queries/GetRestaurantsByNameQuery.cs
+++ src/zindler.web/Core/Queries/GetRestaurantsByNameQuery.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.Linq;
 using AutoMapper;
 using YelpSharp;
+using YelpSharp.Data.Options;
 using zindler.web.Models;
 
 namespace zindler.web.Core.Queries
@@ -18,7 +19,7 @@ namespace zindler.web.Core.Queries
 			var consumerSecret = ConfigurationManager.AppSettings["Yelp.ConsumerSecret"];
 
 			var yelp = new Yelp(new Options { AccessToken = accessToken, AccessTokenSecret = accessTokenSecret, ConsumerKey = consumerKey, ConsumerSecret = consumerSecret });
-			var results = yelp.Search(name, "houston, tx").Result;
+			var results = yelp.Search(new SearchOptions{GeneralOptions = new GeneralOptions{category_filter = "food", term = name}, LocationOptions = new LocationOptions{location = "houston, tx"}}).Result;
 
 			return results.businesses.Select(Mapper.Map<Restaurant>).ToList();
 		}

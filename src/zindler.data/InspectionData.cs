29e48d69be5ed019b32c80659b7cfa49a8389560
David Savage <david@headspring.com>
2013-05-18 12:37:12 -0500

    Added GeoLookup to data

--- src/zindler.data/InspectionData.cs
+++ src/zindler.data/InspectionData.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using CsvHelper;
+using Newtonsoft.Json;
 
 namespace zindler.data
 {
@@ -19,22 +20,23 @@ namespace zindler.data
                 new CsvReader(new StreamReader(Path.Combine(Environment.CurrentDirectory, "src", "inspection-data.csv")));
 
             var records = csv.GetRecords<InspectionRecord>();
-            //using (var client = new HttpClient())
-            //{
-            //    client.BaseAddress = new Uri("https://maps.googleapis.com");
-            //    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json")); 
-            //    string queryFormat = "maps/api/geocode/json?address={0}+{1},{2},{3}&sensor=false";
-            //    foreach (var record in records)
-            //    {
-            //        var response =
-            //            client.GetAsync(string.Format(queryFormat, record.StNum, record.StName.Replace(" ", "+"),
-            //                                                record.City, record.StName)).Result;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://maps.googleapis.com");
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                string queryFormat = "maps/api/geocode/json?address={0}+{1},{2},{3}&sensor=false";
+                foreach (var record in records)
+                {
+                    var response =
+                        client.GetAsync(string.Format(queryFormat, record.StNum, record.StName.Replace(" ", "+"),
+                                                            record.City, record.StName)).Result;
 
-            //        var result = response.Content.ReadAsStringAsync().Result;
+                    var result = JsonConvert.DeserializeObject<GoogleResult>(response.Content.ReadAsStringAsync().Result);
+                    if (!string.IsNullOrEmpty(result.status) && result.status == "OK")
+                        record.Geometry = result.results[0].geometry.location;
 
-            //        Console.Write("");
-            //    }
-            //}
+                }
+            }
             return records;
         }
 

fb4dca2941c0853e80ac875296584633c283b070
Jose Basilio <jose@headspring.com>
2013-05-18 10:04:58 -0500

    Basic layout and some data for the search page

--- src/zindler.web/App/routerConfiguration.js
+++ src/zindler.web/App/routerConfiguration.js
@@ -1,7 +1,11 @@
 ﻿define(['durandal/plugins/router'], function (router) {
 	router.useConvention();
 
+	router.mapNav("/", "viewmodels/search");
+
 	router.mapNav("search");
+
+	router.mapNav("about");
 	
 	router.mapNav("search/:name", "viewmodels/searchResult", "Search Results");
 	router.mapNav("details/:name", "viewmodels/restaurantDetail", "Restaurant Detail");

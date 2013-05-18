defb20ef1b037345aabc3b049ed895ea5ac2f115
David Savage <david@headspring.com>
2013-05-18 12:04:05 -0500

    cleanup

--- src/zindler.web/App_Start/MapConfig.cs
+++ src/zindler.web/App_Start/MapConfig.cs
@@ -7,12 +7,8 @@ namespace zindler.web.App_Start
     {
         public static void Configure()
         {
-<<<<<<< HEAD
             MapConfig.Configure();
-            
-=======
 			  Mapper.Initialize(cfg => cfg.AddProfile<ZindlerProfile>());
->>>>>>> b0e126520659f16b6e98afdd91ff6f316ccc4cea
         }
     }
 }

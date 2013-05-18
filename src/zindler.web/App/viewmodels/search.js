91ada0b013517c4fd45e1633d36948503b59b806
Jose Basilio <jose@headspring.com>
2013-05-18 12:28:12 -0500

    Interactive search now works

--- src/zindler.web/App/viewmodels/search.js
+++ src/zindler.web/App/viewmodels/search.js
@@ -1,15 +1,23 @@
 ﻿define(['durandal/system', 'durandal/plugins/router'],
     function (system, router) {
-    	var data = [{ name: 'Chipotle', address: '1022 Main street' }, { name: 'Brendas Mexican', address: '7763 Westheimer rd.' }];
+    	var data = [{ name: 'Chipotle', address: '1022 Main street' },
+    		{ name: 'Brendas Mexican', address: '7763 Westheimer rd.' }];
+
     	return {
     		router: router,
     		activate: function () {
     			system.log("Search View Activated!!");
     		},
-    		showResults: function (text) {
-    			system.log('restaurant: ' + typeof (text));
-    			this.restaurants(data);
+    		showResults: function () {
+    			var self = this;
+    			var name = this.searchText();
+    			system.log(name);
+
+    			$.get('/api/search?name=' + name).done(function(results) {
+    				self.restaurants(results);
+    			});
     		},
-    		restaurants: ko.observableArray([])
+    		restaurants: ko.observableArray([]),
+    		searchText: ko.observable("")
     	};
     });

9d17d07d8518cdb19fe20ff20c70c72b8fabf184
David Savage <david@headspring.com>
2013-05-18 11:22:59 -0500

    Added automapper

--- src/zindler.web/Global.asax.cs
+++ src/zindler.web/Global.asax.cs
@@ -23,6 +23,7 @@ namespace zindler
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
+            MapConfig.Configure();
 		}
 	}
 }

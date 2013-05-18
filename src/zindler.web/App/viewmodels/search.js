bec0618af17c94af5c6d5a836826fa0039cf9fd5
Jose Basilio <jose@headspring.com>
2013-05-18 16:30:39 -0500

    Fixed the keyup issue when searching. Changed the bootstrap theme. Added our info the about page.

--- src/zindler.web/App/viewmodels/search.js
+++ src/zindler.web/App/viewmodels/search.js
@@ -1,9 +1,6 @@
 ﻿define(['durandal/system', 'durandal/plugins/router'],
     function (system, router) {
-    	var data = [{ name: 'Chipotle', address: '1022 Main street' },
-    		{ name: 'Brendas Mexican', address: '7763 Westheimer rd.' }];
-
-    	return {
+    	var viewModel = {
     		router: router,
     		activate: function () {
     			system.log("Search View Activated!!");
@@ -13,11 +10,22 @@
     			var name = this.searchText();
     			system.log(name);
 
-    			$.get('/api/search?name=' + name).done(function(results) {
+    			$.get('/api/search?name=' + name).done(function (results) {
     				self.restaurants(results);
     			});
     		},
     		restaurants: ko.observableArray([]),
     		searchText: ko.observable("")
     	};
+
+    	$(document).on('keyup', '#searchBox', function () {
+    		var name = $(this).val();
+    		system.log(name);
+
+    		$.get('/api/search?name=' + name).done(function (results) {
+    			viewModel.restaurants(results);
+    		});
+    	});
+
+    	return viewModel;
     });
